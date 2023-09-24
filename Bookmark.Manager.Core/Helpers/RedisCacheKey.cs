bbd34e56f858bc33b9cf4498fd24bf17e7700bc3
Taras Iskiv <taras.iskiv@gmail.com>
2023-09-24 15:03:29 +0200

    Designed logic for the redis keys

---
+++ Bookmark.Manager.Core/Helpers/RedisCacheKey.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Bookmark.Manager.Core.Helpers
+{
+	/*
+	 Redis cache keys logic:
+		AppName/UserId/Folders/ParentFolderId; ParentFolderId can be null
+		AppName/UserId/Bookmarks/ParentFolderId; ParentBookmarkId cant be null
+	 */
+	public enum RedisCacheKey
+	{
+		Folders,
+		Bookmarks
+	}
+}
+
